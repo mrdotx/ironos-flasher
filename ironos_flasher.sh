5b9fb80df95f712e7d2c0f46965c7473b5164c76
mrdotx <klassiker@gmx.de>
2021-10-29 14:40:35 +0200

    added command line option for maximum attempts until successfully flash the device

--- ironos_flasher.sh
+++ ironos_flasher.sh
@@ -3,24 +3,26 @@
 # path:   /home/klassiker/.local/share/repos/ironos-flasher/ironos_flasher.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/ironos-flasher
-# date:   2021-10-28T19:21:10+0200
+# date:   2021-10-29T14:37:23+0200
 
 script=$(basename "$0")
 help="$script [-h/--help] -- script for flashing firmware to compatible devices
   Usage:
-    $script <hexfile>
+    $script [-a/--attempts] <hexfile>
 
   Settings:
-    <hexfile> = file to be flashed to the device
+    [-a/--attempts] = maximum attempts until successfully flash the device
+                      (default: 2)
+    <hexfile>       = file to be flashed to the device
 
   Examples:
-    $script TS100_EN.hex"
+    $script TS100_EN.hex
+    $script -a 1 TS100_EN.hex
+    $script --attempts 5 TS100_EN.hex"
 
 mnt_dir="/tmp/ironos"
-max_retries=2
 gautomount=0
 instructions=0
-result=
 
 disable_gautomount() {
     which gsettings > /dev/null \
@@ -78,58 +80,83 @@ umount_device() {
     rm -d "$mnt_dir"
 }
 
+check_file() {
+    if [ ! -f "$1" ] \
+        || [ "$(head -c1 "$1")" != ":" ] \
+        || [ "$(tail -n1 "$1" | head -c1)" != ":" ]; then
+            printf "%s\n\n  %s\n    '%s' %s\n    %s\n" \
+                "$help" \
+                "Error:" \
+                "$1" \
+                "doesn't look like a valid HEX file." \
+                "Please provide a regular HEX file to flash..."
+            exit 1
+    fi
+}
+
+check_int() {
+    ! [ "$1" -eq "$1" ] 2> /dev/null \
+        && printf "%s\n\n  %s\n    '%s' %s\n" \
+            "$help" \
+            "Error:" \
+            "$1" \
+            "doesn't look like a valid integer." \
+        && exit 1
+}
+
 flash_device() {
-    disable_gautomount
+    max_attempts=$2
+    while [ "$max_attempts" -ge 1 ]; do
+        disable_gautomount
 
-    wait_for_device
-    printf "\nFound config disk device on %s\n" "$device"
+        wait_for_device
+        printf "\nFound config disk device on %s\n" "$device"
 
-    mount_device
-    printf "Mounted config disk drive, flashing...\n"
-    cat "$1" > "$mnt_dir/firmware.hex"
+        mount_device
+        printf "Mounted config disk drive, flashing...\n"
+        cat "$1" > "$mnt_dir/firmware.hex"
 
-    printf "Remounting config disk drive\n"
-    umount_device
-    wait_for_device
-    mount_device
+        printf "Remounting config disk drive\n"
+        umount_device
+        wait_for_device
+        mount_device
 
-    result=$(find "$mnt_dir" -type f -iname 'firmware.*' \
-        | cut -d"/" -f4 \
-        | tail -n1 \
-    )
+        result=$(find "$mnt_dir" -type f -iname 'firmware.*' \
+            | cut -d"/" -f4 \
+            | tail -n1 \
+        )
 
-    umount_device
-    enable_gautomount
+        umount_device
+        enable_gautomount
+
+        if [ "$result" = "firmware.rdy" ]; then
+            max_attempts=0
+            printf "\n  Flashing successful!\n"
+        else
+            max_attempts=$((max_attempts-1))
+            printf "\n  Flashing error! Try again %d more time(s)...\n" \
+                "$max_attempts"
+        fi
+    done
 }
 
 case "$1" in
     -h | --help | "")
         printf "%s\n" "$help"
         ;;
-    *)
-        if [ ! -f "$1" ] \
-            || [ "$(head -c1 "$1")" != ":" ] \
-            || [ "$(tail -n1 "$1" | head -c1)" != ":" ]; then
-                printf "%s\n\n  %s\n    '%s' %s\n    %s\n" \
-                    "$help" \
-                    "Error:" \
-                    "$1" \
-                    "doesn't look like a valid HEX file." \
-                    "Please provide a regular HEX file to flash..."
-                exit 1
-        fi
+    -a | --attempts )
+        check_int "$2"
+        retries=$2
+        shift 2
 
-        while [ $max_retries -ge 1 ]; do
-            flash_device "$1"
-            if [ "$result" = "firmware.rdy" ]; then
-                max_retries=0
-                printf "\n  Flashing successful!\n"
-            else
-                max_retries=$((max_retries-1))
-                printf "\n  Flashing error! Try again %d more time...\n" \
-                    "$max_retries"
-            fi
-        done
+        check_file "$1"
+        flash_device "$1" "$retries"
+
+        printf "\n"
+        ;;
+    *)
+        check_file "$1"
+        flash_device "$1" 2
 
         printf "\n"
         ;;
